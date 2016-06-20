bbc0e02608b68c3ea8dbf87bb1d5e5f06319bdfa
Adam Clements <aj.clements@live.co.uk>
2016-06-20 14:00:50 +0100

    Riddles

---
+++ Riddles/Riddles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Riddles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What comes down, but never goes up?");
+            string answer = Console.ReadLine();
+
+            while (!answer.Contains("rain"))
+            {
+                Console.WriteLine("Try again");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine("You got it!");
+            Console.WriteLine("I'm tall when I'm young and short when I'm old. What am I?");
+
+            do
+            {
+                answer = Console.ReadLine();
+                if(!answer.Contains("candle"))
+                {
+                    Console.WriteLine("Try again");
+                }
+                
+            } while (!answer.Contains("candle"));
+
+            Console.WriteLine("You got both correct! well done!");
+            Console.WriteLine("Please press enter twice to exit");
+            Console.ReadLine();
+        }
+    }
+}
