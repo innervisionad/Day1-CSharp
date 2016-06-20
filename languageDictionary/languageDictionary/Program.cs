23a62f4634ceab8ba5c1eaa0b6c7b535203df990
Adam Clements <aj.clements@live.co.uk>
2016-06-20 16:14:19 +0100

    LanguageDictionary

---
+++ languageDictionary/languageDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace languageDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> languageDict = new Dictionary<string, string>();
+            languageDict.Add("C#", "Pretty useful language");
+            languageDict.Add("Python", "user friendly, slithery language");
+            languageDict.Add("ruby", "expensive language");
+            Console.WriteLine("Welcome to the language library.");
+            string lang = Console.ReadLine();
+
+        }
+    }
+}
