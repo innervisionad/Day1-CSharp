8b288890f4035597fac10ec415c0d932406fef66
Adam Clements <aj.clements@live.co.uk>
2016-06-20 14:26:04 +0100

    seven dwarves for and foreach loop

---
+++ sevenDwarves/sevenDwarves/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sevenDwarves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] sevenDwarves = { "Happy", "Doc", "Sleepy", "Sneezy", "Dopey", "Bashful", "Grumpy" };
+
+            string food;
+            string drink;
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(sevenDwarves[i] + ", What would you like to eat?");
+                food = Console.ReadLine();
+                Console.WriteLine(sevenDwarves[i] + ", Your " + food + " is ready!");
+            }
+            
+            foreach (string dwarf in sevenDwarves)
+            {
+                
+                Console.WriteLine(dwarf + ", What would you like to drink?");
+                drink = Console.ReadLine();
+                Console.WriteLine(dwarf + ", your " + drink + " is ready!");
+            }
+
+
+        }
+    }
+}
